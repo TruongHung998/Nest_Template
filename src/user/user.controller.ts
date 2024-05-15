1d0ce8a462b46418a754d6e9b141a20f44ea6c13
Mac <mac@MBP-16inch-1TB.lan>
2024-05-15 23:26:11 +0700

    _: create user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Delete, Get } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller("user")
+@Controller('user')
 export class UserController {
-  @Get()
-  findAll(): string {
-    return 'test';
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
   }
-  @Delete("/by-id")
-  deleteUser(): string {
-    return "";
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
   }
 }
