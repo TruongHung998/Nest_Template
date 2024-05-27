b8e2ed493d39a3c335821c4aba524d07ea300c5e
HungEplus <truonghung239810@gmail.com>
2024-05-27 16:29:55 +0700

    _: reinit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,29 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { MongooseModule } from "@nestjs/mongoose";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { UserModule } from "./user/user.module";
-import { StatefulModule } from "./stateful/stateful.module";
-import { StatelessModule } from "./stateless/stateless.module";
+import {Module} from "@nestjs/common";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
+import {UserModule} from "./user/user.module";
+import {AuthModule} from './auth/auth.module';
+
 @Module({
-  imports: [
-    UserModule,
-    StatefulModule,
-    StatelessModule,
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>("MONGO_DB"),
-      }),
-      inject: [ConfigService],
-    }),
-    ConfigModule.forRoot({
-      envFilePath: ".env",
-      isGlobal: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get<string>("MONGO_DB"),
+            }),
+            inject: [ConfigService],
+        }),
+        ConfigModule.forRoot({
+            envFilePath: ".env",
+            isGlobal: true,
+        }),
+        UserModule,
+        AuthModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
